3c2b938cfeb288f9346878277ea1de3da24f744b
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-04-25 17:37:21 +0500

    Add position into button.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@ let elButton2 = document.getElementById('arrows-btn_two');
 let elButton3 = document.getElementById('arrows-btn_three');
 let elButton4 = document.getElementById('arrows-btn_four');
 let elButton5 = document.getElementById('arrows-btn_five');
+
 let elCloselabel1 = document.getElementById('label-wrapper-id_one');
 let elCloselabel2 = document.getElementById('label-wrapper-id_two');
 let elCloselabel3 = document.getElementById('label-wrapper-id_three');
 let elCloselabel4 = document.getElementById('label-wrapper-id_four');
 let elCloselabel5 = document.getElementById('label-wrapper-id_five');
 
+let elInput = document.querySelectorAll('input[type="radio"]');
+const elText1 = document.querySelector('.black-text_one');
+const elText2 = document.querySelector('.black-text_two');
+const elText3 = document.querySelector('.black-text_three');
+const elText4 = document.querySelector('.black-text_four');
+const elText5 = document.querySelector('.black-text_five');
+let a  = document.querySelectorAll('.checkbox-input-heading')
+
 /* elbutton listener */
 elButton1.addEventListener("click", function () {
-  elButton1.classList.toggle("arrows-button-rotate");
+  elButton1.classList.toggle("arrows-button_first-rotate");
   elCloselabel1.classList.toggle("open");
 });
 
@@ -36,12 +45,32 @@ elButton5.addEventListener("click", function () {
 });
 
 
-let elInput = document.querySelectorAll('input[type="radio"]');
-const elText1 = document.querySelector('.black-text_one');
-const elText2 = document.querySelector('.black-text_two');
-const elText3 = document.querySelector('.black-text_three');
-const elText4 = document.querySelector('.black-text_four');
-const elText5 = document.querySelector('.black-text_five');
+// TEXT START
+a[0].addEventListener("click", function () {
+  elButton1.classList.toggle("arrows-button_first-rotate");
+  elCloselabel1.classList.toggle("open");
+});
+
+a[1].addEventListener("click", function () {
+  elButton2.classList.toggle("arrows-button-rotate");
+  elCloselabel2.classList.toggle("label-wrapper-close");
+});
+
+a[2].addEventListener("click", function () {
+  elButton3.classList.toggle("arrows-button-rotate");
+  elCloselabel3.classList.toggle("label-wrapper-close");
+});
+
+a[3].addEventListener("click", function () {
+  elButton4.classList.toggle("arrows-button-rotate");
+  elCloselabel4.classList.toggle("label-wrapper-close");
+});
+
+a[4].addEventListener("click", function () {
+  elButton5.classList.toggle("arrows-button-rotate");
+  elCloselabel5.classList.toggle("label-wrapper-close");
+});
+//  TEXT END
 
 /* Input change */
 elInput.forEach(function (value,index){
